1339ca3da87ed15498452645f4bbe84f89c58240
Jan Šik <honzik.sik@seznam.cz>
2023-11-19 20:25:10 +0100

    html sites

--- pomodoro/app/urls.py
+++ pomodoro/app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path("", views.home, name="home")
+    path('', views.home, name='home'),
+    path('stats/', views.stats_view, name='stats'),
+    path('timer/', views.timer_view, name='timer'),  # Assuming you have a timer_view in views.py
+    path('shop/', views.shop_view, name='shop'),     # Assuming you have a shop_view in views.py
+    # ... other url patterns ...
 ]
--- pomodoro/app/views.py
+++ pomodoro/app/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+
+def stats_view(request):
+    context = {
+        'total_completed': 10,  # Example data
+        'time_focused': '5 hours',
+        'time_paused': '1 hour',
+        'tomatoes': 50,
+        'money': '$25'
+    }
+    return render(request, 'stats.html', context)
+
+
+def timer_view(request):
+    # Your code here
+    return render(request, 'timer.html')
+
+
+def shop_view(request):
+    # Your code here
+    return render(request, 'shop.html')
