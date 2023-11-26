dfba86fa82fab10b07fab8681ad2647c66fe2604
Jan Šik <honzik.sik@seznam.cz>
2023-11-26 18:07:29 +0100

    more

---
+++ pomodoro/app/static/js.js
@@ -0,0 +1,7 @@
+//on click start button show the text about pomodoro
+//do it mmore times
+document.querySelector('#startButton').onclick = () => {
+    let text = document.querySelector('#more_text');
+    //show the text
+    text.style.display = 'block';
+}
